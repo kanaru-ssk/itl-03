95eb86525e6d00675a8db0aada5ff4d5031914b9
kanaru-ssk <2019january.0131@gmail.com>
2022-02-28 23:19:54 +0900

    ymlのフォーマット解除

--- src/model/AuthModel.tsx
+++ src/model/AuthModel.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useContext, useEffect } from "react";
+import React, { createContext, useState, useEffect } from "react";
 import { Auth, getAuth, onAuthStateChanged } from "firebase/auth";
 
 type AuthContextProps = Auth | null;
@@ -15,7 +15,7 @@ export const AuthProvider = ({ children }: Props) => {
     const auth = getAuth();
 
     useEffect(() => {
-        const unsubscribed = onAuthStateChanged(auth, (user) => {
+        const unsubscribed = onAuthStateChanged(auth, () => {
             setUser(auth);
         });
         return () => {
