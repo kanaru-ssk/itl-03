f1cb439c7b559d2eb86741b6278cc98eb9e85a0b
kanaru-ssk <2019january.0131@gmail.com>
2022-03-21 21:22:36 +0900

    ヘッダーナビ追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 // React Router取得
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import { useContext } from 'react';
 
+import { AuthContext } from './model/AuthModel';
 // コンポーネント取得
 import Home from './view/Home';
 import User from './view/User';
@@ -8,8 +10,15 @@ import Explore from './view/Explore';
 
 // ルーティング
 const App = () => {
+	const user = useContext(AuthContext);
 	return (
 		<BrowserRouter>
+			<nav>
+				<Link to="/">home</Link>&nbsp;
+				<Link to="/explore">explore</Link>&nbsp;
+				<Link to={'/' + user.dbUser?.user_id}>mypage</Link>
+			</nav>
+
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/explore" element={<Explore />} />
