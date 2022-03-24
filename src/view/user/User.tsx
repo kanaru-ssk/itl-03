3d642e82fcfec1337604ecb0aca392164f60695b
kanaru-ssk <2019january.0131@gmail.com>
2022-03-24 11:50:25 +0900

    Items component作成

--- src/view/user/User.tsx
+++ src/view/user/User.tsx
@@ -7,6 +7,8 @@ import { AuthContext } from '../../model/AuthModel';
 import { getUserDataByUserId } from '../../model/UserModel';
 import { getItems } from '../../model/itemModel';
 
+import Items from './Items';
+
 // 匿名認証 => ユーザーページ
 // 表示userとログインuserが異なる => ユーザーページ
 // 表示userとログインuserが同じ => マイページ
@@ -14,8 +16,6 @@ import { getItems } from '../../model/itemModel';
 const User = () => {
 	const { paramsUid } = useParams();
 	const user = useContext(AuthContext);
-	const authUser = user.authUser;
-	const dbUser = user.dbUser;
 
 	const [paramsUser, setParamsUser] = useState<dbUser | undefined>(undefined);
 	const [items, setItems] = useState<item[]>([]);
@@ -27,74 +27,23 @@ const User = () => {
 		});
 	}, [paramsUid]);
 
-	if (dbUser?.user_id === paramsUid) {
-		return (
+	return (
+		<div>
+			<h1>User</h1>
+			<div>パラメーターuid : {paramsUid}</div>
+			{user.dbUser?.user_id === paramsUid ? <h2>マイページ</h2> : <h2>ユーザーページ</h2>}
+
+			<h3>ユーザー情報</h3>
 			<div>
-				<h1>User</h1>
-				<h2>マイページ</h2>
-				<div>パラメーターuid : {paramsUid}</div>
-
-				<h3>ログイン情報</h3>
-				<div>
-					<img src={dbUser?.user_icon} alt="" />
-					<div>{dbUser?.user_name}</div>
-					<div>{dbUser?.user_bio}</div>
-					<div>{dbUser?.user_twitter_disp_id}</div>
-				</div>
-
-				<h3>items</h3>
-				<ul>
-					{items.map((value, key) => {
-						return (
-							<li key={key}>
-								{value.item_name}&nbsp;
-								{value.item_caption}
-							</li>
-						);
-					})}
-				</ul>
+				<img src={paramsUser?.user_icon} alt="" />
+				<div>{paramsUser?.user_name}</div>
+				<div>{paramsUser?.user_bio}</div>
+				<div>{paramsUser?.user_twitter_disp_id}</div>
 			</div>
-		);
-	} else {
-		return (
-			<div>
-				<h1>User</h1>
-				<h2>ユーザーページ</h2>
-				<div>パラメーターuid : {paramsUid}</div>
 
-				<h3>ログイン情報</h3>
-				<div>認証状態 : {authUser ? '認証済み' : '未認証'}</div>
-				<div>認証種別 : {authUser?.isAnonymous ? '匿名' : 'twitterログイン'}</div>
-				<div>ログインuid : {authUser?.uid}</div>
-				<div>
-					<img src={dbUser?.user_icon} alt="" />
-					<div>{dbUser?.user_name}</div>
-					<div>{dbUser?.user_bio}</div>
-					<div>{dbUser?.user_twitter_disp_id}</div>
-				</div>
-
-				<h3>ユーザー情報</h3>
-				<div>
-					<img src={paramsUser?.user_icon} alt="" />
-					<div>{paramsUser?.user_name}</div>
-					<div>{paramsUser?.user_bio}</div>
-					<div>{paramsUser?.user_twitter_disp_id}</div>
-				</div>
-
-				<h3>items</h3>
-				<ul>
-					{items.map((value, key) => {
-						return (
-							<li key={key}>
-								{value.item_name}&nbsp;
-								{value.item_caption}
-							</li>
-						);
-					})}
-				</ul>
-			</div>
-		);
-	}
+			<Items items={items} />
+		</div>
+	);
 };
 
 export default User;
