5e351c73e5ee91ef4b7d71559dc89508677814e4
kanaru-ssk <2019january.0131@gmail.com>
2022-03-24 14:40:27 +0900

    画像読み込み

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,14 @@
 // css取得
 import './App.scss';
 
+// 画像取得
+import logoImg from './img/logo.svg';
+import userImg from './img/user.svg';
+import homeImg from './img/home.svg';
+import exploreImg from './img/explore.svg';
+import noticeImg from './img/notice.svg';
+import messageImg from './img/message.svg';
+
 // React取得
 import { useContext } from 'react';
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
@@ -23,6 +31,13 @@ const App = () => {
 	if (user?.authUser) {
 		return (
 			<BrowserRouter>
+				<header>
+					<img src={logoImg} alt="" />
+					<Link to={'/' + user.dbUser?.user_id}>
+						<img src={userImg} alt="" />
+					</Link>
+				</header>
+
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/explore" element={<Explore />} />
@@ -30,11 +45,31 @@ const App = () => {
 					<Route path="/:paramsUid" element={<User />} />
 				</Routes>
 
-				<nav>
-					<Link to="/">home</Link>&nbsp;
-					<Link to="/explore">explore</Link>&nbsp;
-					<Link to={'/' + user.dbUser?.user_id}>mypage</Link>
-				</nav>
+				<footer>
+					<nav>
+						<ul>
+							<li>
+								<Link to="/">
+									<img src={homeImg} alt="" />
+								</Link>
+							</li>
+
+							<li>
+								<Link to="/explore">
+									<img src={exploreImg} alt="" />
+								</Link>
+							</li>
+
+							<li>
+								<img src={noticeImg} alt="" />
+							</li>
+
+							<li>
+								<img src={messageImg} alt="" />
+							</li>
+						</ul>
+					</nav>
+				</footer>
 			</BrowserRouter>
 		);
 	} else {
