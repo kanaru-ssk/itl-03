711b9e222fb4193bc5434f8ec9e36529d55564f0
kanaru-ssk <2019january.0131@gmail.com>
2022-03-24 18:58:02 +0900

    Header component 作成

---
+++ src/view/components/modules/Header.tsx
@@ -0,0 +1,28 @@
+// ロード画面
+
+import './Header.scss';
+
+import logoImg from 'img/logo.svg';
+import userImg from 'img/user.svg';
+
+import { useContext, useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
+
+import { AuthContext } from 'model/AuthModel';
+
+const Header = () => {
+	const user = useContext(AuthContext);
+
+	return (
+		<header>
+			<Link to={'/'}>
+				<img className="header__item" src={logoImg} alt="logo" />
+			</Link>
+			<Link to={'/' + user.dbUser?.user_id}>
+				<img className="header__item" src={userImg} alt="user-icon" />
+			</Link>
+		</header>
+	);
+};
+
+export default Header;
