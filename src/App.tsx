c3c5beb616025309cc8a9fc63e718fcb1e78561a
kanaru-ssk <2019january.0131@gmail.com>
2022-03-24 19:00:55 +0900

    header footer をcomponentとして切り分け

--- src/App.tsx
+++ src/App.tsx
@@ -3,75 +3,39 @@
 // css取得
 import './App.scss';
 
-// 画像取得
-import logoImg from 'img/logo.svg';
-import userImg from 'img/user.svg';
-import homeImg from 'img/home.svg';
-import exploreImg from 'img/explore.svg';
-import noticeImg from 'img/notice.svg';
-import messageImg from 'img/message.svg';
-
 // React取得
-import { useContext } from 'react';
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
-
-// モデル取得
-import { AuthContext } from 'model/AuthModel';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 // コンポーネント取得
-import Home from 'view/pages/Home';
-import User from 'view/pages/User';
-import Explore from 'view/pages/Explore';
 import Loading from 'view/components/modules/Loading';
+import Header from 'view/components/modules/Header';
+import Footer from 'view/components/modules/Footer';
+import Home from 'view/pages/Home';
+import Explore from 'view/pages/Explore';
+import Notice from 'view/pages/Notice';
+import Message from 'view/pages/Message';
+import User from 'view/pages/User';
 
 // ルーティング
 const App = () => {
-	const user = useContext(AuthContext);
-
 	return (
 		<BrowserRouter>
-			<header>
-				<img src={logoImg} alt="" />
-				<Link to={'/' + user.dbUser?.user_id}>
-					<img src={userImg} alt="" />
-				</Link>
-			</header>
+			<Header />
 
 			<Loading />
-			{/* {user?.authUser ? '' : <Loading />} */}
 
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/explore" element={<Explore />} />
-				<Route path="/explore/:placeId" element={<Explore />} />
-				<Route path="/:paramsUid" element={<User />} />
-			</Routes>
+			<main>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/explore" element={<Explore />} />
+					<Route path="/explore/:placeId" element={<Explore />} />
+					<Route path="/notice" element={<Notice />} />
+					<Route path="/message" element={<Message />} />
+					<Route path="/:paramsUid" element={<User />} />
+				</Routes>
+			</main>
 
-			<footer>
-				<nav>
-					<ul>
-						<li>
-							<Link to="/">
-								<img src={homeImg} alt="" />
-							</Link>
-						</li>
-
-						<li>
-							<Link to="/explore">
-								<img src={exploreImg} alt="" />
-							</Link>
-						</li>
-
-						<li>
-							<img src={noticeImg} alt="" />
-						</li>
-
-						<li>
-							<img src={messageImg} alt="" />
-						</li>
-					</ul>
-				</nav>
-			</footer>
+			<Footer />
 		</BrowserRouter>
 	);
 };
