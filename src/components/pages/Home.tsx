d76afe412c407b4c8af461e84444763f7ddf4f2f
kanaru-ssk <2019january.0131@gmail.com>
2022-03-25 15:45:14 +0900

    home の表示編集

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -8,23 +8,13 @@ import Login from 'components/organisms/Login';
 
 const Home = () => {
 	const user = useContext(AuthContext);
-	const authUser = user.authUser;
-	const dbUser = user.dbUser;
 
-	if (authUser?.isAnonymous) {
+	if (user.authUser?.isAnonymous) {
 		return <Login />;
 	} else {
 		return (
 			<main>
 				<h1>Home</h1>
-				<div>tiwtter認証済みなのでホームを表示</div>
-				<div>ログインuid : {authUser?.uid}</div>
-				<div>
-					<img src={dbUser?.user_icon} alt="" />
-					<div>{dbUser?.user_name}</div>
-					<div>{dbUser?.user_bio}</div>
-					<div>{dbUser?.user_twitter_disp_id}</div>
-				</div>
 				<button onClick={logout}>ログアウト</button>
 			</main>
 		);
