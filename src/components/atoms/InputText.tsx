996422708e03c90deb4e7477fbceca2bdf92b7b0
kanaru-ssk <2019january.0131@gmail.com>
2022-03-27 01:41:00 +0900

    inputのborder-radiusを8pxに統一+変数作成

--- src/components/atoms/InputText.tsx
+++ src/components/atoms/InputText.tsx
@@ -1,14 +1,14 @@
 // テキストinput
 
 // css取得
-// import style from './InputText.module.scss';
+import style from './InputText.module.scss';
 
 type Props = {
 	onChange: (e: any) => void;
 };
 
 const InputText = ({ onChange }: Props) => {
-	return <input type="text" onChange={(e) => onChange(e.target.value)} />;
+	return <input className={style.input} type="text" onChange={(e) => onChange(e.target.value)} />;
 };
 
 export default InputText;
