2223b05f5f6bfa39aca7e6df26323057c32b91ac
kanaru-ssk <2019january.0131@gmail.com>
2022-03-27 00:52:14 +0900

    ログインボタン作成

---
+++ src/components/atoms/ButtonLogin.tsx
@@ -0,0 +1,18 @@
+// ボタン
+
+import style from './Button.module.scss';
+
+type Props = {
+	onClick: (event: any) => void;
+	children: React.ReactNode;
+};
+
+const Button = ({ onClick, children }: Props) => {
+	return (
+		<button className={style.button} onClick={(e) => onClick(e)}>
+			{children}
+		</button>
+	);
+};
+
+export default Button;
