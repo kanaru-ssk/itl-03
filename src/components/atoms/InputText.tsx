09ed7e772092de3a5cf859285ba5e245cc6fbf3d
kanaru-ssk <2019january.0131@gmail.com>
2022-03-27 02:21:14 +0900

    フォーカスした時Sliderが上にいくよう調整

--- src/components/atoms/InputText.tsx
+++ src/components/atoms/InputText.tsx
@@ -4,11 +4,21 @@
 import style from './InputText.module.scss';
 
 type Props = {
+	placeholder: string;
+	onFocus: () => void;
 	onChange: (e: any) => void;
 };
 
-const InputText = ({ onChange }: Props) => {
-	return <input className={style.input} type="text" onChange={(e) => onChange(e.target.value)} />;
+const InputText = ({ placeholder, onFocus, onChange }: Props) => {
+	return (
+		<input
+			className={style.input}
+			type="text"
+			placeholder={placeholder}
+			onFocus={onFocus}
+			onChange={(e) => onChange(e.target.value)}
+		/>
+	);
 };
 
 export default InputText;
