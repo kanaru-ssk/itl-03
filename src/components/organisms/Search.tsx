cc42ea3b1e766a57a356417f4228048be104bcbd
kanaru-ssk <2019january.0131@gmail.com>
2022-03-27 00:56:39 +0900

    Input,Resultをコンポーネント分割

--- src/components/organisms/Search.tsx
+++ src/components/organisms/Search.tsx
@@ -1,57 +1,39 @@
 // 検索
 
-import { useContext, useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+// React取得
+import { useEffect, useState } from 'react';
 
-import { AuthContext, loginWithTwitter } from 'model/AuthModel';
-import { searchMap, convertPlace } from 'model/PlaceModel';
-import { createPost } from 'model/PostModel';
+// モデル取得
+import { searchMap } from 'model/PlaceModel';
+
+// コンポーネント取得
+import Slider from 'components/molecules/Slider';
+import SearchInput from './SearchInput';
+import SearchResult from './SearchResult';
+import InputText from 'components/atoms/InputText';
+import InputRadio from 'components/atoms/InputRadio';
 
 const Search = () => {
-	const user = useContext(AuthContext);
+	const [isOpen, setIsOpen] = useState<boolean>(false);
 	const [inputText, setInputText] = useState<string>('');
 	const [queryText, setQueryText] = useState<string>('');
 	const [placeType, setPlaceType] = useState<placeType>('');
-	const [places, setPlaces] = useState<google.maps.places.PlaceResult[]>([]);
+	const [placeResults, setPlaceResult] = useState<google.maps.places.PlaceResult[]>([]);
 
 	useEffect(() => {
-		if (queryText !== '' || placeType !== '') searchMap(queryText, placeType).then((result) => setPlaces(result));
+		if (queryText !== '' || placeType !== '') {
+			searchMap(queryText, placeType).then((result) => setPlaceResult(result));
+		}
 	}, [placeType, queryText]);
 
-	const onAddPost = (place: place): void => {
-		if (user.authUser?.isAnonymous) {
-			loginWithTwitter();
-		} else {
-			createPost(user.dbUser, place);
-		}
-	};
-
 	return (
-		<div>
-			<input type="text" value={inputText} onChange={(e) => setInputText(e.target.value)} />
+		<Slider isOpen={isOpen} setIsOpen={setIsOpen}>
+			<InputText onChange={setInputText} />
 			<button onClick={() => setQueryText(inputText)}>検索</button>
-			<br />
-			<button onClick={() => setPlaceType('cafe')}>カフェ</button>
-			<button onClick={() => setPlaceType('restaurant')}>レストラン</button>
-			<button onClick={() => setPlaceType('bar')}>バー</button>
-			<button onClick={() => setPlaceType('library')}>図書館</button>
-			<button onClick={() => setPlaceType('art_gallery')}>美術館</button>
-			<button onClick={() => setPlaceType('aquarium')}>水族館</button>
-			<button onClick={() => setPlaceType('park')}>公園</button>
-			<button onClick={() => setPlaceType('movie_theater')}>映画館</button>
-			<button onClick={() => setPlaceType('lodging')}>宿泊</button>
+			<SearchInput placeType={placeType} setPlaceType={setPlaceType} />
 
-			<ul>
-				{places.map((place, key) => {
-					return (
-						<li key={key}>
-							<Link to={'/explore/' + place.place_id}>{place.name}</Link>
-							<button onClick={() => onAddPost(convertPlace(place))}>追加</button>
-						</li>
-					);
-				})}
-			</ul>
-		</div>
+			<SearchResult placeResults={placeResults} />
+		</Slider>
 	);
 };
 
