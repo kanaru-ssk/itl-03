c9e51f8f98cbe37abcd16b1b35fadd761d92749d
kanaru-ssk <2019january.0131@gmail.com>
2022-03-26 19:13:49 +0900

    不要なimport削除

--- src/components/organisms/UserHeader.tsx
+++ src/components/organisms/UserHeader.tsx
@@ -3,12 +3,6 @@
 // 画像取得
 import menuIgm from 'img/menu.svg';
 
-// React取得
-import { useContext } from 'react';
-
-// model取得
-import { AuthContext } from 'model/AuthModel';
-
 // コンポーネント取得
 import Header from 'components/atoms/Header';
 import UserId from 'components/atoms/UserId';
@@ -18,8 +12,6 @@ type Props = {
 };
 
 const UserHeader = ({ paramsUid }: Props) => {
-	const user = useContext(AuthContext);
-
 	return (
 		<Header>
 			<UserId>{paramsUid}</UserId>
