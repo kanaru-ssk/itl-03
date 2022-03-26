3f8f88d9f0898fced5e301de0b715c92d4d9424d
kanaru-ssk <2019january.0131@gmail.com>
2022-03-27 00:50:55 +0900

    InputRadio component 作成

---
+++ src/components/atoms/InputRadio.tsx
@@ -0,0 +1,44 @@
+// ラジオinput (2回クリックで選択解除)
+
+// css取得
+import style from './InputRadio.module.scss';
+
+// React取得
+import { useRef } from 'react';
+
+type Props = {
+	value: string;
+	setValue: (e: any) => void;
+	before: placeType;
+};
+
+const InputRadio = ({ value, setValue, before }: Props) => {
+	const ref = useRef<HTMLInputElement>(null);
+
+	const onClick = (e: any) => {
+		if (e.target.value === before && ref.current) {
+			ref.current.checked = false;
+			setValue('');
+		} else {
+			setValue(e.target.value);
+		}
+	};
+	return (
+		<div>
+			<input
+				className={style.radio}
+				type="radio"
+				name="place-type"
+				value={value}
+				id={value}
+				onClick={onClick}
+				ref={ref}
+			/>
+			<label htmlFor={value} className={style.label}>
+				{value}
+			</label>
+		</div>
+	);
+};
+
+export default InputRadio;
