f97833d6524e5cd5ee6eb244979be819ef97684a
kanaru-ssk <2019january.0131@gmail.com>
2022-03-27 22:00:55 +0900

    Review component 作成

---
+++ src/components/molecules/Review.tsx
@@ -0,0 +1,32 @@
+// レビューヘッド
+
+// css取得
+import style from './Review.module.scss';
+
+// component取得
+import UserIcon from 'components/atoms/UserIcon';
+import UserName from 'components/atoms/UserName';
+import Stars from 'components/atoms/Stars';
+import ReviewText from 'components/atoms/ReviewText';
+
+type Props = {
+	review: google.maps.places.PlaceReview;
+};
+
+const Review = ({ review }: Props) => {
+	return (
+		<div>
+			<div className={style.container}>
+				<UserIcon src={review.profile_photo_url} />
+				<UserName>{review.author_name}</UserName>
+				<Stars rating={review.rating} />
+				{review.relative_time_description}
+			</div>
+			<div className={style.text}>
+				<ReviewText text={review.text} />
+			</div>
+		</div>
+	);
+};
+
+export default Review;
