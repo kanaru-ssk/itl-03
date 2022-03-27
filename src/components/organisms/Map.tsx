5fcad0d15c94a3c3b372f915f77ddf19ac9e112f
kanaru-ssk <2019january.0131@gmail.com>
2022-03-27 16:19:02 +0900

    cssをmoduleに変更

--- src/components/organisms/Map.tsx
+++ src/components/organisms/Map.tsx
@@ -1,16 +1,19 @@
 // ログインページ
 
-import './Map.scss';
+// css取得
+import style from './Map.module.scss';
 
+// react取得
 import { useEffect } from 'react';
 
+// model取得
 import { initMap } from 'model/PlaceModel';
 
 const Login = () => {
 	useEffect(() => {
 		initMap();
 	}, []);
-	return <div id="map"></div>;
+	return <div id="map" className={style.map}></div>;
 };
 
 export default Login;
