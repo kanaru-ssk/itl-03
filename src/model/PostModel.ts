7788daf58bd747ab6e4598ef744c0f4a391d0a6c
kanaru-ssk <2019january.0131@gmail.com>
2022-03-30 01:21:17 +0900

    フィールド変更

--- src/model/PostModel.ts
+++ src/model/PostModel.ts
@@ -18,7 +18,9 @@ export const getPostsByUserId = async (user_id: string | undefined): Promise<pos
 			place_id: doc.data().place_id,
 			place_name: doc.data().place_name,
 			place_type: doc.data().place_type,
-			place_photos: doc.data().place_photos,
+			place_photo: doc.data().place_photo,
+
+			post_images: doc.data().post_images,
 			post_caption: doc.data().post_caption,
 			post_checked: doc.data().post_checked,
 
@@ -42,15 +44,14 @@ export const createPost = async (user: dbUser, place: place) => {
 	const newPost: post = {
 		at_created: serverTimestamp(),
 		at_updated: serverTimestamp(),
-		at_checked: null,
 
 		place_id: place.place_id,
 		place_name: place.place_name,
 		place_type: place.place_type,
-		place_photos: place.place_photos,
+		place_photo: place.place_photo,
 
+		post_images: null,
 		post_caption: 'キャプション',
-		post_checked: false,
 
 		user_uid: user.user_uid,
 		user_id: user.user_id,
