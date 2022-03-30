ad0e532073b8020a79b199767e1919143bfb6d89
kanaru-ssk <2019january.0131@gmail.com>
2022-03-31 02:27:21 +0900

    Modaltem component作成

---
+++ src/components/atoms/ModalItem.tsx
@@ -0,0 +1,19 @@
+// Modalアイテム
+
+// css取得
+import style from './ModalItem.module.scss';
+
+type Props = {
+	text: string;
+	onClick: () => void;
+};
+
+const ModalItem = ({ text, onClick }: Props) => {
+	return (
+		<div className={style.item} onClick={onClick}>
+			{text}
+		</div>
+	);
+};
+
+export default ModalItem;
