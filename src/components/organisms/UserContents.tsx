10dc8e07a77ee9c2e0a7ad752f0f332c18685980
kanaru-ssk <2019january.0131@gmail.com>
2022-04-01 02:39:45 +0900

    CheckedList ページ作成

--- src/components/organisms/UserContents.tsx
+++ src/components/organisms/UserContents.tsx
@@ -2,6 +2,7 @@
 
 // component取得
 import List from 'components/organisms/List';
+import CheckedList from 'components/organisms/CheckedList';
 
 // 匿名認証 => ユーザーページ
 // 表示userとログインuserが異なる => ユーザーページ
@@ -16,7 +17,7 @@ const UserContents = ({ uid, tab }: Props) => {
 	if (tab === 'list') {
 		return <List uid={uid} />;
 	} else if (tab === 'checkedList') {
-		return <div>checkedLis</div>;
+		return <CheckedList uid={uid} />;
 	} else {
 		return <div>posts</div>;
 	}
