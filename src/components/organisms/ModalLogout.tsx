a2de762246b2a9772fdbc3dbf8874c028add0d08
kanaru-ssk <2019january.0131@gmail.com>
2022-03-31 22:09:44 +0900

    ログアウトmodal作成

---
+++ src/components/organisms/ModalLogout.tsx
@@ -0,0 +1,28 @@
+// ログアウトModal
+
+// model取得
+import { logout } from 'model/AuthModel';
+
+// hooks取得
+import { useModal } from 'hooks/Modal';
+
+// css取得
+import style from './ModalLogout.module.scss';
+
+const ModalLogout = () => {
+	const modal = useModal();
+	return (
+		<div className={style.container}>
+			<div>ログアウトしますか？</div>
+
+			<div className={style.logout} onClick={logout}>
+				ログアウト
+			</div>
+			<div className={style.cancel} onClick={() => modal(null)}>
+				キャンセル
+			</div>
+		</div>
+	);
+};
+
+export default ModalLogout;
