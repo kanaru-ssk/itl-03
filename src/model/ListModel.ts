f11c8eb7f7bc917c58eeb060ac9732abafb26897
kanaru-ssk <2019january.0131@gmail.com>
2022-04-01 00:11:40 +0900

    is_checkedでフィルター追加

--- src/model/ListModel.ts
+++ src/model/ListModel.ts
@@ -4,10 +4,15 @@
 export const getList = async (uid: string | undefined): Promise<item[]> => {
 	if (uid === undefined) return [];
 
-	const { getFirestore, collection, getDocs, query, orderBy, limit } = await import('firebase/firestore');
+	const { getFirestore, collection, getDocs, query, where, orderBy, limit } = await import('firebase/firestore');
 
 	const db = getFirestore();
-	const queryRef = query(collection(db, 'users', uid, 'list'), orderBy('at_created', 'desc'), limit(20));
+	const queryRef = query(
+		collection(db, 'users', uid, 'list'),
+		where('is_checked', '==', false),
+		orderBy('at_created', 'desc'),
+		limit(20)
+	);
 	const querySnap = await getDocs(queryRef);
 
 	const items: item[] = querySnap.docs.map((doc) => {
@@ -15,8 +20,8 @@ export const getList = async (uid: string | undefined): Promise<item[]> => {
 			at_created: doc.data().at_created,
 			at_checked: doc.data().at_checked,
 
-			item_iid: doc.id,
-			item_is_checked: doc.data().is_checked,
+			doc_id: doc.id,
+			is_checked: doc.data().is_checked,
 
 			user_uid: doc.data().user_uid,
 			user_twitter_sys_id: doc.data().user_twitter_sys_id,
@@ -38,11 +43,11 @@ export const createItem = async (user: dbUser | undefined, place: place) => {
 
 	const db = getFirestore();
 
-	const newItem: Omit<item, 'item_iid'> = {
+	const newItem: Omit<item, 'doc_id'> = {
 		at_created: serverTimestamp(),
 		at_checked: null,
 
-		item_is_checked: false,
+		is_checked: false,
 
 		user_uid: user.user_uid,
 		user_twitter_sys_id: user.user_twitter_sys_id,
@@ -56,9 +61,20 @@ export const createItem = async (user: dbUser | undefined, place: place) => {
 	addDoc(collection(db, 'users', user.user_uid, 'list'), newItem);
 };
 
+export const checkItem = async (item: item) => {
+	const { getFirestore, doc, updateDoc, serverTimestamp } = await import('firebase/firestore');
+
+	const db = getFirestore();
+	updateDoc(doc(db, 'users', item.user_uid, 'list', item.doc_id), {
+		is_checked: true,
+		at_checked: serverTimestamp()
+	});
+	console.log('check : ' + item.place_name);
+};
+
 export const deleteItem = async (item: item) => {
 	const { getFirestore, doc, deleteDoc } = await import('firebase/firestore');
 
 	const db = getFirestore();
-	deleteDoc(doc(db, 'users', item.user_uid, 'list', item.item_iid));
+	deleteDoc(doc(db, 'users', item.user_uid, 'list', item.doc_id));
 };
