3541a97681d3536fa502604de520f2524fd3dc7b
kanaru-ssk <2019january.0131@gmail.com>
2022-03-31 15:44:04 +0900

    削除実装

--- src/components/organisms/List.tsx
+++ src/components/organisms/List.tsx
@@ -4,20 +4,32 @@
 import style from './Posts.module.scss';
 
 // react取得
-import { useState, useEffect } from 'react';
+import { useContext, useState, useEffect } from 'react';
 
 // model取得
-import { getList } from 'model/ListModel';
+import { AuthContext } from 'model/AuthModel';
+import { getList, deleteItem } from 'model/ListModel';
 
 // component取得
 import Item from 'components/molecules/Item';
+import { ModalContext } from 'components/organisms/Modal';
+import ListMenu from 'components/organisms/ListMenu';
 
 type Props = {
 	uid: string;
 };
 
 const Posts = ({ uid }: Props) => {
+	const user = useContext(AuthContext);
+	const modal = useContext(ModalContext);
 	const [list, setList] = useState<item[]>([]);
+
+	const onContextMenu = (e: any, item: item) => {
+		e.preventDefault();
+		if (user.authUser?.isAnonymous) return;
+
+		modal(<ListMenu item={item} list={list} setList={setList} />);
+	};
 
 	useEffect(() => {
 		getList(uid).then((result) => {
@@ -29,7 +41,12 @@ const Posts = ({ uid }: Props) => {
 		<ul className={style.container}>
 			{list.map((item, key) => {
 				return (
-					<li key={key}>
+					<li
+						key={key}
+						onContextMenu={(e) => {
+							onContextMenu(e, item);
+						}}
+					>
 						<Item item={item} />
 					</li>
 				);
