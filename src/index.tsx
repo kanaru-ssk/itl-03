5aff6d96cc76ea0ccc2020f42b41118e572d5ca7
kanaru-ssk <2019january.0131@gmail.com>
2022-03-31 22:09:30 +0900

    ModalProvider追加

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,7 @@ import { initFirebase } from 'model/InitModel';
 // hooks取得
 import { AuthProvider } from 'hooks/Auth';
 import { SliderProvider } from 'hooks/Slider';
+import { ModalProvider } from 'hooks/Modal';
 
 // component取得
 import App from 'App';
@@ -15,13 +16,15 @@ import App from 'App';
 // firebase初期化
 initFirebase();
 
-// authをcontextで全体に共有
+// contextを全体に共有
 ReactDOM.render(
 	<React.StrictMode>
 		<AuthProvider>
-			<SliderProvider>
-				<App />
-			</SliderProvider>
+			<ModalProvider>
+				<SliderProvider>
+					<App />
+				</SliderProvider>
+			</ModalProvider>
 		</AuthProvider>
 	</React.StrictMode>,
 	document.getElementById('root')
