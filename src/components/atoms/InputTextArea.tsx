df8923171a7c245e5efeb04e1d431dfdeb541747
kanaru-ssk <2019january.0131@gmail.com>
2022-04-01 11:46:54 +0900

    InputTextArea component作成

---
+++ src/components/atoms/InputTextArea.tsx
@@ -0,0 +1,46 @@
+// テキストエリアinput
+
+// react取得
+import { useEffect, useRef } from 'react';
+import { text } from 'stream/consumers';
+// css取得
+import style from './InputText.module.scss';
+
+type Props = {
+	value: string;
+	onInput: (e: any) => void;
+	placeholder: string;
+};
+
+const InputTextArea = ({ placeholder, value, onInput }: Props) => {
+	const textareaRef = useRef<HTMLTextAreaElement>(null);
+	useEffect(() => {
+		if (textareaRef.current) {
+			if (textareaRef.current.offsetHeight < textareaRef.current.scrollHeight) {
+				textareaRef.current.style.height = textareaRef.current.scrollHeight + 'px';
+			}
+		}
+	}, [value]);
+
+	const onInputText = (e: any) => {
+		onInput(e.target.value);
+		if (textareaRef.current) {
+			if (textareaRef.current.offsetHeight < textareaRef.current.scrollHeight) {
+				textareaRef.current.style.height = textareaRef.current.scrollHeight + 'px';
+			}
+		}
+	};
+
+	return (
+		<textarea
+			className={style.input}
+			placeholder={placeholder}
+			onInput={onInputText}
+			defaultValue={value}
+			value={value}
+			ref={textareaRef}
+		></textarea>
+	);
+};
+
+export default InputTextArea;
