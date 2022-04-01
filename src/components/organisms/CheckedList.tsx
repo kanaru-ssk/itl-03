3e13bae549fe487fae0280680e5ca584e101f347
kanaru-ssk <2019january.0131@gmail.com>
2022-04-02 01:56:56 +0900

    もっと読み込む機能実装

--- src/components/organisms/CheckedList.tsx
+++ src/components/organisms/CheckedList.tsx
@@ -1,10 +1,13 @@
 // リスト一覧
 
+// firebase取得
+import { Timestamp } from 'firebase/firestore';
+
 // react取得
 import { useState, useEffect } from 'react';
 
 // model取得
-import { getList } from 'model/ListModel';
+import { getList, getOldestItem } from 'model/ListModel';
 
 // hooks取得
 import { useAuth } from 'hooks/Auth';
@@ -25,6 +28,19 @@ const CheckedList = ({ uid }: Props) => {
 	const user = useAuth();
 	const slider = useSlider();
 	const [list, setList] = useState<item[]>([]);
+	const [oldest, setOldest] = useState<item | null>(null);
+
+	const now = Timestamp.now();
+	const hasMore = oldest ? !Boolean(list.find((i) => i.doc_id === oldest.doc_id)) : false;
+
+	useEffect(() => {
+		getOldestItem(uid, true).then((result) => {
+			setOldest(result);
+		});
+		getList(uid, true, now).then((results) => {
+			setList(results);
+		});
+	}, [uid]);
 
 	const onContextMenu = (e: any, item: item) => {
 		e.preventDefault();
@@ -33,27 +49,31 @@ const CheckedList = ({ uid }: Props) => {
 		slider(<ListMenu item={item} list={list} setList={setList} />);
 	};
 
-	useEffect(() => {
-		getList(uid, true).then((result) => {
-			setList(result);
+	const onMoreLoad = () => {
+		const last = list[list.length - 1].at_created;
+		getList(uid, true, last).then((results) => {
+			setList([...list, ...results]);
 		});
-	}, [uid]);
+	};
 
 	return (
-		<ul className={style.container}>
-			{list.map((item, key) => {
-				return (
-					<li
-						key={key}
-						onContextMenu={(e) => {
-							onContextMenu(e, item);
-						}}
-					>
-						<CheckedItem item={item} />
-					</li>
-				);
-			})}
-		</ul>
+		<div>
+			<ul className={style.container}>
+				{list.map((item, key) => {
+					return (
+						<li
+							key={key}
+							onContextMenu={(e) => {
+								onContextMenu(e, item);
+							}}
+						>
+							<CheckedItem item={item} />
+						</li>
+					);
+				})}
+			</ul>
+			{hasMore && <div onClick={onMoreLoad}>more</div>}
+		</div>
 	);
 };
 
--- src/components/organisms/List.tsx
+++ src/components/organisms/List.tsx
@@ -1,10 +1,13 @@
 // リスト一覧
 
+// firebase取得
+import { Timestamp } from 'firebase/firestore';
+
 // react取得
 import { useState, useEffect } from 'react';
 
 // model取得
-import { getList } from 'model/ListModel';
+import { getList, getOldestItem } from 'model/ListModel';
 
 // hooks取得
 import { useAuth } from 'hooks/Auth';
@@ -25,6 +28,19 @@ const List = ({ uid }: Props) => {
 	const user = useAuth();
 	const slider = useSlider();
 	const [list, setList] = useState<item[]>([]);
+	const [oldest, setOldest] = useState<item | null>(null);
+
+	const now = Timestamp.now();
+	const hasMore = oldest ? !Boolean(list.find((i) => i.doc_id === oldest.doc_id)) : false;
+
+	useEffect(() => {
+		getOldestItem(uid, false).then((results) => {
+			setOldest(results);
+		});
+		getList(uid, false, now).then((results) => {
+			setList(results);
+		});
+	}, [uid]);
 
 	const onContextMenu = (e: any, item: item) => {
 		e.preventDefault();
@@ -33,27 +49,31 @@ const List = ({ uid }: Props) => {
 		slider(<ListMenu item={item} list={list} setList={setList} />);
 	};
 
-	useEffect(() => {
-		getList(uid, false).then((result) => {
-			setList(result);
+	const onMoreLoad = () => {
+		const last = list[list.length - 1].at_created;
+		getList(uid, false, last).then((results) => {
+			setList([...list, ...results]);
 		});
-	}, [uid]);
+	};
 
 	return (
-		<ul className={style.container}>
-			{list.map((item, key) => {
-				return (
-					<li
-						key={key}
-						onContextMenu={(e) => {
-							onContextMenu(e, item);
-						}}
-					>
-						<Item item={item} />
-					</li>
-				);
-			})}
-		</ul>
+		<div>
+			<ul className={style.container}>
+				{list.map((item, key) => {
+					return (
+						<li
+							key={key}
+							onContextMenu={(e) => {
+								onContextMenu(e, item);
+							}}
+						>
+							<Item item={item} />
+						</li>
+					);
+				})}
+			</ul>
+			{hasMore && <div onClick={onMoreLoad}>more</div>}
+		</div>
 	);
 };
 
