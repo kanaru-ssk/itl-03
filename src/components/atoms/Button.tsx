e5f507b9c2917db62d4a6efb2551992848b609c6
kanaru-ssk <2019january.0131@gmail.com>
2022-04-06 20:51:40 +0900

    引数からchildren削除

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -5,13 +5,13 @@ import style from './Button.module.scss';
 
 type Props = {
 	onClick: (event: any) => void;
-	children: React.ReactNode;
+	text: string;
 };
 
-const Button = ({ onClick, children }: Props) => {
+const Button = ({ onClick, text }: Props) => {
 	return (
 		<button className={style.button} onClick={(e) => onClick(e)}>
-			{children}
+			{text}
 		</button>
 	);
 };
