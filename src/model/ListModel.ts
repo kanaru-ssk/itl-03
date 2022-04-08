377d954311b7e04775b1d4505f1d52c6ba36d3a8
kanaru-ssk <2019january.0131@gmail.com>
2022-04-09 02:24:17 +0900

    場所のアイコンをbase64形式で保存

--- src/model/ListModel.ts
+++ src/model/ListModel.ts
@@ -11,7 +11,7 @@ export const getOldestItem = async (uid: string | undefined, isChecked: boolean)
 		collection(db, 'users', uid, 'list'),
 		where('is_checked', '==', isChecked),
 		orderBy('at_created', 'asc'),
-		limit(1)
+		limit(1),
 	);
 	const querySnap = await getDocs(queryRef);
 
@@ -29,7 +29,7 @@ export const getOldestItem = async (uid: string | undefined, isChecked: boolean)
 			place_id: querySnap.docs[0].data().place_id,
 			place_name: querySnap.docs[0].data().place_name,
 			place_type: querySnap.docs[0].data().place_type,
-			place_photo: querySnap.docs[0].data().place_photo
+			place_photo: querySnap.docs[0].data().place_photo,
 		};
 
 		return item;
@@ -43,7 +43,7 @@ export const getList = async (
 	uid: string | undefined,
 	isChecked: boolean,
 	start: Timestamp | FieldValue,
-	limitNum: number
+	limitNum: number,
 ): Promise<item[]> => {
 	if (uid === undefined) return [];
 
@@ -57,7 +57,7 @@ export const getList = async (
 		where('is_checked', '==', isChecked),
 		orderBy('at_created', 'desc'),
 		startAfter(start),
-		limit(limitNum)
+		limit(limitNum),
 	);
 
 	const querySnap = await getDocs(queryRef);
@@ -76,7 +76,7 @@ export const getList = async (
 			place_id: doc.data().place_id,
 			place_name: doc.data().place_name,
 			place_type: doc.data().place_type,
-			place_photo: doc.data().place_photo
+			place_photo: doc.data().place_photo,
 		};
 	});
 
@@ -86,10 +86,19 @@ export const getList = async (
 // アイテムデータ作成
 export const createItem = async (user: dbUser | undefined, place: place) => {
 	if (!user) return;
+
+	// const { getFunctions, httpsCallable } = await import('firebase/functions');
+	// const functions = getFunctions();
+	// functions.region = 'asia-northeast1';
+	// const get = httpsCallable(functions, 'getPhotoUrl');
+	// const result: any = await get({ url: place.place_photo });
+	// const resultURL: RequestInfo = result.data;
+	const photoURL: RequestInfo = await getPhotoURL(place.place_photo);
+	const photo: string = await getImageBase64(photoURL);
+
 	const { getFirestore, addDoc, collection, serverTimestamp } = await import('firebase/firestore');
 
 	const db = getFirestore();
-
 	const newItem: Omit<item, 'doc_id'> = {
 		at_created: serverTimestamp(),
 		at_checked: null,
@@ -102,7 +111,7 @@ export const createItem = async (user: dbUser | undefined, place: place) => {
 		place_id: place.place_id,
 		place_name: place.place_name,
 		place_type: place.place_type,
-		place_photo: place.place_photo
+		place_photo: photo,
 	};
 
 	addDoc(collection(db, 'users', user.user_uid, 'list'), newItem);
@@ -115,7 +124,7 @@ export const checkItem = async (item: item) => {
 	const db = getFirestore();
 	updateDoc(doc(db, 'users', item.user_uid, 'list', item.doc_id), {
 		is_checked: true,
-		at_checked: serverTimestamp()
+		at_checked: serverTimestamp(),
 	});
 };
 
@@ -125,4 +134,30 @@ export const deleteItem = async (item: item) => {
 
 	const db = getFirestore();
 	deleteDoc(doc(db, 'users', item.user_uid, 'list', item.doc_id));
+};
+
+const getPhotoURL = async (url: string): Promise<RequestInfo> => {
+	const { getFunctions, httpsCallable } = await import('firebase/functions');
+	const functions = getFunctions();
+	functions.region = 'asia-northeast1';
+	const get = httpsCallable(functions, 'getPhotoUrl');
+	const result: any = await get({ url: url });
+	const resultURL: RequestInfo = result.data;
+	return resultURL;
+};
+
+// 画像をbase64stringに変換
+const getImageBase64 = async (url: RequestInfo | null) => {
+	if (url === null) return '';
+	const response = await fetch(url);
+	const contentType = response.headers.get('content-type');
+	const arrayBuffer = await response.arrayBuffer();
+	const APPLY_MAX = 1024;
+	let encodedStr = '';
+	// ArrayBufferの中身を1024バイトに区切って少しずつ文字列にしていく
+	for (let i: number = 0; i < arrayBuffer.byteLength; i += APPLY_MAX) {
+		encodedStr += String.fromCharCode.apply(null, [...new Uint8Array(arrayBuffer.slice(i, i + APPLY_MAX))]);
+	}
+	let base64String = window.btoa(encodedStr);
+	return `data:${contentType};base64,${base64String}`;
 };
